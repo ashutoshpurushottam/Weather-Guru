ea104386d9390eae2086a9c71e5b442dc1b27f96
Ashutosh Purushottam <mailtoapurushottam@gmail.com>
2021-09-13 21:38:00 +0530

    first commit

---
+++ app/src/main/java/com/deft/weatherguru/ui/activities/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.deft.weatherguru.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.deft.weatherguru.R
+import com.deft.weatherguru.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navController = findNavController(R.id.nav_fragment)
+        setUpNavigation()
+    }
+
+    private fun setUpNavigation() {
+        // control for bottom navigation
+        bottom_navigation.setupWithNavController(navController)
+        // action bar clicks
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    // Behaviour on clicking the up button
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
+    }
+}
